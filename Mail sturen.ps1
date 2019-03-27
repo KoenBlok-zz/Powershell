ce9f7214b3974f5787e567087f5562547435ef01
KoenBlok <koenblok1@hotmail.com>
2019-03-27 11:37:53 +0100

    Create Mail sturen.ps1

---
+++ Mail sturen.ps1
@@ -0,0 +1,17 @@
+﻿# Name		    : Mail sturen
+# Script URI	:
+# Description	: Stuurt een emailtje
+# Version	    : 0.0.1
+# Author	    : Koen Blok
+
+#alle variabelen
+$SmtpServer = read-host -prompt 'vul de smtp server in'
+$SmtpUser = Read-Host -Prompt 'input jou email'
+$MailtTo = read-host -prompt 'naar wie'
+$MailFrom = $SmtpUser
+$MailSubject = read-host -prompt 'voor het onderwerp in'
+$MailBody = read-host -prompt 'voer de body in'
+$Credentials = Get-Credential
+
+#stuurt de mail met alle informatie van de variabelen
+Send-MailMessage -To $MailtTo -from $MailFrom -Subject $MailSubject -Body $MailBody -SmtpServer $SmtpServer -Credential $Credentials -Port 587
